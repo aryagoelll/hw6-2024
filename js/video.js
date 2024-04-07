77ea13cc6888f48d610fe191f49cd6d5991ee103
aryagoelll <156857489+aryagoelll@users.noreply.github.com>
2024-04-06 20:45:38 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,20 @@
+
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+
+var video = document.getElementById('player1');
+
+video.autoplay = false;
+
+video.loop = false;
+
+});
+
+// document.querySelector("#play").addEventListener("click", function() {
+// 	console.log("Play Video");
+// });
+
